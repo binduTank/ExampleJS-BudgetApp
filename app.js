88ce29f7dcce68bd9a9c8c5191e39714109c7da3
bindu.tank@asteria.co.in <bindu.tank@asteria.co.in>
2019-01-05 19:49:28 +0530

    Displays Budget on UI

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ var budgetController = (function() {
 			if(type === 'exp') {
 				newItem = new Expense(ID, des, val);
 			} else if (type === 'inc') {
-				newItem = new Expense(ID, des, val);
+				newItem = new Income(ID, des, val);
 			}
 
 			// Push it into data structure
@@ -107,7 +107,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	};
 
 	return {
@@ -157,6 +161,20 @@ var UIController = (function() {
         	fieldsArr[0].focus();
         },
 
+        displyBudget: function(obj) {
+
+        	document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+        	document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+        	document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+        	if(obj.percentage > 0) {
+        		document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+        	} else {
+        		document.querySelector(DOMstrings.percentageLabel).textContent = '------';
+        	}
+
+        },
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -191,6 +209,7 @@ var controller = (function(budgetctrl, UIctrl) {
 		var budget = budgetctrl.getBudget();
 
 		// Display the Budget on UI 
+		UIctrl.displyBudget(budget);
 		console.log(budget);
 	}
 
@@ -223,6 +242,12 @@ var controller = (function(budgetctrl, UIctrl) {
 	return {
 		init: function() {
 			console.log(' Application has started');
+			UIctrl.displyBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListener();
 		}
 	}
